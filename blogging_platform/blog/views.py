b2cd559ac5393e0fb625640fb604f37ffc2c1650
Mwandikwajay <musangij@gmail.com>
2024-12-15 20:43:13 +0300

    Added authentication

--- blogging_platform/blog/views.py
+++ blogging_platform/blog/views.py
@@ -1,18 +1,45 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
-@api_view(['GET'])
-def list_posts(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+@api_view(['GET', 'POST'])
+def list_or_create_posts(request):
+    if request.method == 'GET':
+        # Handle GET: List all posts
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        # Handle POST: Create a new post
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
-@api_view(['POST'])
-def create_post(request):
-    serializer = PostSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, id):
+    try:
+        post = Post.objects.get(pk=id)
+    except Post.DoesNotExist:
+        return Response({"error": "Post not found"}, status=404)
+
+    if request.method == 'GET':
+        # Handle GET: Retrieve a single post
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        # Handle PUT: Update a post
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        # Handle DELETE: Delete a post
+        post.delete()
+        return Response({"message": "Post deleted successfully"}, status=204)
